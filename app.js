76246d1bd6e6c11056c4b85f91b6c3a4c0f1d353
mark <markpackham1@gmail.com>
2023-05-09 10:39:13 +0100

    use template engine
    https://youtu.be/-foo92lFIto?t=746

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
 
 const express = require("express");
+const expressLayout = require("express-ejs-layouts");
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
+
+// Template Engine
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Hello World");
