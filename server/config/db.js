d77be2bbc3e7f4291afebdc75b7c8a9c2a3832f1
mark <markpackham1@gmail.com>
2023-05-11 14:26:05 +0100

    have database connected
    https://youtu.be/FjuctFNN0FA?t=753

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,10 @@ const { default: mongoose } = require("mongoose");
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
+    // what the .env file would have
+    // MONGODB_URI=mongodb://127.0.0.1:27017/blogExpressEjs
+    // an example with parameters
+    // mongoose.connect('mongodb://username:password@host:port/database?options...');
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`Database connected: ${conn.connection.host}`);
   } catch (error) {
