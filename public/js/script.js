3b876387c742760db77ce1edb62bfddf37cb7d2c
mark <markpackham1@gmail.com>
2023-05-15 10:40:10 +0100

    loop through buttons
    https://youtu.be/9Bnpl6bcev4?t=1036

--- public/js/script.js
+++ public/js/script.js
@@ -2,4 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const allButtons = document.querySelectorAll(".searchBtn");
   const searchBar = document.querySelectorAll(".searchBar");
   const searchInput = document.getElementById("searchInput");
+  const searchClose = document.getElementById("searchClose");
+
+  for (let i = 0; i < allButtons.length; i++) {
+    allButtons[i].addEventListener("click", function () {
+      searchBar.style.visibility = "visible";
+      searchBar.classList.add("open");
+    });
+  }
 });
