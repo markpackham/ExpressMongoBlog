f8e4c12c8ac885b541db95ae26c5b1d04190e284
mark <markpackham1@gmail.com>
2023-05-15 10:43:52 +0100

    show search bar when search clicked on
    https://youtu.be/9Bnpl6bcev4?t=1127

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const allButtons = document.querySelectorAll(".searchBtn");
-  const searchBar = document.querySelectorAll(".searchBar");
+  const searchBar = document.querySelector(".searchBar");
   const searchInput = document.getElementById("searchInput");
   const searchClose = document.getElementById("searchClose");
 
@@ -8,6 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
     allButtons[i].addEventListener("click", function () {
       searchBar.style.visibility = "visible";
       searchBar.classList.add("open");
+      this.setAttribute("aria-expanded", "true");
+      searchInput.focus();
     });
   }
 });
