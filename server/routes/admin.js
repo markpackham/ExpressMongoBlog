a875e99b3d9def6e63532fe45e5c97a532a2d8fa
mark <markpackham1@gmail.com>
2023-05-16 11:45:11 +0100

    add header admin view
    https://youtu.be/FmqBk2UTHzE?t=187

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -5,13 +5,14 @@ const Post = require("../models/Post");
 // Routes
 
 // GET
+// Admin Login
 router.get("/admin", async (req, res) => {
   try {
     const locals = {
       title: "Admin",
       description: "Administrator section",
     };
-    res.render("admin/index", { locals, data });
+    res.render("admin/index", { locals });
   } catch (error) {
     console.log(error);
   }
