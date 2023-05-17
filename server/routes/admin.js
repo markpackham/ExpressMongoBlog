47adbaa3475d0a1355779ffe86e29753292b84fd
mark <markpackham1@gmail.com>
2023-05-17 18:09:36 +0100

    use Json Web Token secret
    https://youtu.be/uCQirwe5UVg?t=759

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const adminLayout = "../views/layouts/admin";
-// Routes
+const jwtSecret = process.env.JWT_SECRET;
 
 // GET
 // Admin Login
@@ -27,9 +27,23 @@ router.get("/admin", async (req, res) => {
 router.post("/admin", async (req, res) => {
   try {
     const { username, password } = req.body;
-    console.log(req.body);
 
-    res.redirect("/admin");
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    // Don't give a hacker hints that the password is wrong or the username is wrong
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // JWT_SECRET=MySecretBlog from .env
+    const token = jwt.sign({ userId: user._id }, jwtSecret);
+    res.cookie("token", token, { httpOnly: true });
   } catch (error) {
     console.log(error);
   }
@@ -37,25 +51,25 @@ router.post("/admin", async (req, res) => {
 
 // POST
 // Admin Register
-router.post("/register", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
+// router.post("/register", async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+//     const hashedPassword = await bcrypt.hash(password, 10);
 
-    try {
-      const user = await User.create({ username, password: hashedPassword });
-      res.status(201).json({ message: "User Created", user });
-    } catch (error) {
-      if (error.code === 11000) {
-        // 409 is a Conflict
-        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/409
-        res.status(409).json({ message: "User already in use" });
-      }
-      res.status(500).json({ message: "Internal server error" });
-    }
-  } catch (error) {
-    console.log(error);
-  }
-});
+//     try {
+//       const user = await User.create({ username, password: hashedPassword });
+//       res.status(201).json({ message: "User Created", user });
+//     } catch (error) {
+//       if (error.code === 11000) {
+//         // 409 is a Conflict
+//         // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/409
+//         res.status(409).json({ message: "User already in use" });
+//       }
+//       res.status(500).json({ message: "Internal server error" });
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 module.exports = router;
